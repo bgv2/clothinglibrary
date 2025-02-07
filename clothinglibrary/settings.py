6b95d8cb5237fb0a6f5b5bd79f05e9f03ce2acc6
BGV <26331505+bgv2@users.noreply.github.com>
2025-02-07 11:59:07 -0500

    added allowed host

--- clothinglibrary/settings.py
+++ clothinglibrary/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '***REMOVED***'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '***REMOVED***-cc94fce6e5b5.herokuapp.com',
+]
 
 
 # Application definition
