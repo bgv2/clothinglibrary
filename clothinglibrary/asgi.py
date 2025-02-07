0e5dde31dadef030579ca884c35a873b45ecc1b8
BGV <26331505+bgv2@users.noreply.github.com>
2025-02-07 11:57:17 -0500

    fixed app name

--- clothinglibrary/asgi.py
+++ clothinglibrary/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for clothinglibary project.
+ASGI config for ***REMOVED*** project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clothinglibary.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '***REMOVED***.settings')
 
 application = get_asgi_application()
