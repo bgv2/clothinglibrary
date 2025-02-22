b5a5985553d470c40f50d2b47203f03ed0165f6d
BGV <26331505+bgv2@users.noreply.github.com>
2025-02-22 17:27:11 -0500

    heroku module

--- clothinglibrary/settings.py
+++ clothinglibrary/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -164,3 +165,9 @@ ACCOUNT_EMAIL_VERIFICATION = 'optional'
 LOGIN_REDIRECT_URL = '/'
 SOCIALACCOUNT_LOGIN_ON_GET = True # log in via Google without showing confirmation screen
 
+try:
+    if 'HEROKU' in os.environ:
+        import django_heroku
+        django_heroku.settings(locals())
+except ImportError:
+    found = False
