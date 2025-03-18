503b4877a6591065ccf0d7674db16bb01d140ea7
emma-ylagan <emma@ylagan.com>
2025-03-18 03:27:28 -0400

    Created test suite and 5 tests

---
+++ clothinglibrary/tests/test_loggedin_users.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+User = get_user_model()
+
+class DashboardAccessTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="testuser", password="password")
+        self.dashboard_url = reverse("profile")
+
+    #user not logged in, tries to access profile page
+    def test_logged_out_user(self):
+        response = self.client.get(self.dashboard_url)
+        self.assertEqual(response.status_code, 302) 
+        self.assertIn("/accounts/", response.url)
+
+    #user is logged in, is able to access the profile page
+    def test_logged_in_user(self):
+        self.client.login(username="testuser", password="password") 
+        response = self.client.get(self.dashboard_url)
+        self.assertEqual(response.status_code, 200) 
