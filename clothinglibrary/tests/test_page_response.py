a762d312e079e62bc1e22e34721bff27d6319754
BGV <26331505+bgv2@users.noreply.github.com>
2025-04-29 22:50:56 -0400

    add test for collections page response

--- clothinglibrary/tests/test_page_response.py
+++ clothinglibrary/tests/test_page_response.py
@@ -11,6 +11,11 @@ class PageResponseTests(TestCase):
         response = self.client.get("/catalog/") 
         self.assertEqual(response.status_code, 200)
 
+    #collections page
+    def test_collections_page_loads(self):
+        response = self.client.get("/collections/") 
+        self.assertEqual(response.status_code, 200)
+
     #page doesn't exist
     def test_page_does_not_exist(self):
         response = self.client.get("/nonexistent/")
